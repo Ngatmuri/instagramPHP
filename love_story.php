f4b5b092b99e6993c2495355d0d303fc144f961f
Ngatmuri <142901620+Ngatmuri@users.noreply.github.com>
2023-11-28 20:16:20 +0700

    Create love_story.php

---
+++ love_story.php
@@ -0,0 +1,166 @@
+<?php
+date_default_timezone_set("Asia/JAKARTA");
+
+$cookie = 'mid=ZTR8GgALAAFFu4y-I1Ccj457w35C; ig_did=760252B4-24AD-4D14-8A75-D64224ECD501; ig_nrcb=1; datr=GXw0ZXoWrL-5z6VDGCLP-Jfv; fbm_124024574287414=base_domain=.instagram.com; shbid="3065\0548143927324\0541732486258:01f7b365771650b94fc6734438d1399ced1a00746a02c2fbb6358e10573ce764c1ce2b63"; shbts="1700950258\0548143927324\0541732486258:01f7a5af311a7511ae49a7369f35744fd378304b4ceebd987b40cbd86d493d2b216ef936"; fbsr_124024574287414=Wiyk2Uqd522Y6KGSdagxUKEdt80TXVMeMibdHcff_EA.eyJ1c2VyX2lkIjoiMTAwMDYzNjYyNjE3NzE3IiwiY29kZSI6IkFRQ0F3enNOdWppamtNRWl2eXdjWXNBekVVelZHVFQ3cHNmZ2lTdDlGVTlKbDJvZHYyY3pJdnFsWHdMVzVweHNwVWhwWkZjSmpkQ0V2bTgwMG5zd1Zqa09mUm1LNTI2UWJfeTZpaUJfbW1OQS1FaFZyd0VJUkRKaFNxOXpvTVNtVHFqM2w3ZXlNY0RsdmRKQ05UZ0pvV3dabHdVbWpJWkF6cUp5TDgwQTRkcmJCTHh1TTVTYkNhVFFraVZya2xxd2dUZ1VCYlQ4LUJqYkt6eUR1c25Bd09oc095YXlXdFQ5Q25VX3BGNVFNcDlOOHR2YzlWb0dPdTZiVUdqTm9lVjlzNEVPMm4xd3Ftbmdjamp2eDBZNjk4ZVhPM3pFLU1hclphdUY3WWtKTnFEZzVwRmxocWVIZV9pYjRUQkpWQmdhcmxBLW0xbWJocmxJdHAtVXZ3cjRDdmVtMHlWRERsY05aamF3ZVQyRDJCUjVvdyIsIm9hdXRoX3Rva2VuIjoiRUFBQnd6TGl4bmpZQk8zUEMzM3pDWHlxbUdmMFBvTm1yYmRKdVRNeGhtRkZCSkJSQTI1eUpJRXRIN3M4RkNsblk4ckFrODRKVUlRWVhWUmx6TVRjc003RFBLUERvcVRIZWZnYUFNN1RwSjVVMU1JUWxyYkNmcnZweENySkNaQjF5ZThFZTB5NjJSTURidmpwU1Zpb2duTURMbVpCeHY4VkFUSldMdnpaQXk3WkFmWkFqcm1yYm5STmRXeDhncWgzbGVleHNaRCIsImFsZ29yaXRobSI6IkhNQUMtU0hBMjU2IiwiaXNzdWVkX2F0IjoxNzAxMTc2NjIxfQ; fbsr_124024574287414=Wiyk2Uqd522Y6KGSdagxUKEdt80TXVMeMibdHcff_EA.eyJ1c2VyX2lkIjoiMTAwMDYzNjYyNjE3NzE3IiwiY29kZSI6IkFRQ0F3enNOdWppamtNRWl2eXdjWXNBekVVelZHVFQ3cHNmZ2lTdDlGVTlKbDJvZHYyY3pJdnFsWHdMVzVweHNwVWhwWkZjSmpkQ0V2bTgwMG5zd1Zqa09mUm1LNTI2UWJfeTZpaUJfbW1OQS1FaFZyd0VJUkRKaFNxOXpvTVNtVHFqM2w3ZXlNY0RsdmRKQ05UZ0pvV3dabHdVbWpJWkF6cUp5TDgwQTRkcmJCTHh1TTVTYkNhVFFraVZya2xxd2dUZ1VCYlQ4LUJqYkt6eUR1c25Bd09oc095YXlXdFQ5Q25VX3BGNVFNcDlOOHR2YzlWb0dPdTZiVUdqTm9lVjlzNEVPMm4xd3Ftbmdjamp2eDBZNjk4ZVhPM3pFLU1hclphdUY3WWtKTnFEZzVwRmxocWVIZV9pYjRUQkpWQmdhcmxBLW0xbWJocmxJdHAtVXZ3cjRDdmVtMHlWRERsY05aamF3ZVQyRDJCUjVvdyIsIm9hdXRoX3Rva2VuIjoiRUFBQnd6TGl4bmpZQk8zUEMzM3pDWHlxbUdmMFBvTm1yYmRKdVRNeGhtRkZCSkJSQTI1eUpJRXRIN3M4RkNsblk4ckFrODRKVUlRWVhWUmx6TVRjc003RFBLUERvcVRIZWZnYUFNN1RwSjVVMU1JUWxyYkNmcnZweENySkNaQjF5ZThFZTB5NjJSTURidmpwU1Zpb2duTURMbVpCeHY4VkFUSldMdnpaQXk3WkFmWkFqcm1yYm5STmRXeDhncWgzbGVleHNaRCIsImFsZ29yaXRobSI6IkhNQUMtU0hBMjU2IiwiaXNzdWVkX2F0IjoxNzAxMTc2NjIxfQ; csrftoken=ytas0lEr4gGlK7w5kA1v9GBTOxVKJdpS; ds_user_id=8143927324; sessionid=8143927324%3A0Y0aOwKgKylkcP%3A16%3AAYeDpzMXpHD1WvA48qmQ6qQhC7TRwZ3iYrWV9tAZkA; rur="VLL\0548143927324\0541732712845:01f7d57fb44e5bb56751d6580e12057ca82f5a3e76f7a6c2bd48cc9806da87adecf1c1c6"';
+$csrf_token = null;
+
+if (preg_match('/csrftoken=([^;]+)/', $cookie, $matches)) {
+    $csrf_token = $matches[1];
+}
+
+if ($csrf_token == null) {
+    echo "CSRF Token not found in the cookie.";
+    exit;
+}
+
+$headers = [
+    'Content-Type: application/json;charset=utf-8',
+    'accept-encoding: UTF-8',
+    'authorization: Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
+    'Cookie: ' . $cookie,
+    'x-csrf-token: ' . $csrf_token,
+    'X-Ig-App-Id: 936619743392459'
+];
+
+$getStoriesIg = cURL("https://www.instagram.com/api/v1/feed/reels_tray/?is_following_feed=false", false, $cookie, $headers);
+$datanya = json_decode($getStoriesIg["content"])->tray;
+# print_r($datanya); //data fullnya di sini kalau mau liat
+
+$a =('
+__________                                          .__                
+\______   \_______  ____   ____  ____   ______ _____|__| ____    ____  
+ |     ___/\_  __ \/  _ \_/ ___\/ __ \ /  ___//  ___/  |/    \  / ___\ 
+ |    |     |  | \(  <_> )  \__\  ___/ \___ \ \___ \|  |   |  \/ /_/  >
+ |____|     |__|   \____/ \___  >___  >____  >____  >__|___|  /\___  / 
+                              \/    \/     \/     \/        \//_____/  
+');
+print $a;
+
+$logsFolder = 'folderLogs/';
+
+if (!is_dir($logsFolder)) {
+    mkdir($logsFolder, 0755, true);
+}
+
+foreach ($datanya as $item) {
+    $username = $item->user->username;
+    $media_ids = $item->media_ids;
+
+    $logFileName = "$logsFolder/log_$username.txt";
+
+    if (!file_exists($logFileName)) {
+        file_put_contents($logFileName, implode("\n", $media_ids));
+
+        foreach ($media_ids as $media_id) {
+                processMediaId($media_id, $cookie, $csrf_token);
+        }
+
+        echo "Processed media_ids for @$username.
+        ";
+    } else {
+        $existingMediaIds = explode("\n", file_get_contents($logFileName));
+        $newMediaIds = array_diff($media_ids, $existingMediaIds);
+
+        if (!empty($newMediaIds)) {
+            file_put_contents($logFileName, "\n" . implode("\n", $newMediaIds), FILE_APPEND);
+
+            foreach ($newMediaIds as $media_id) {
+                processMediaId($media_id, $cookie, $csrf_token);
+            }
+
+            echo "
+            Processed new media_ids for @$username.";
+        } else {
+            echo "
+            Tidak ada Story baru dari @$username.";
+        }
+    }
+}
+
+function processMediaId($media_id, $cookie, $csrf_token)
+{
+    echo "Processing media_id: $media_id
+    ";
+
+    $headerss   = array(
+        'authority: www.instagram.com',
+        'method: POST',
+        'path: /api/v1/story_interactions/send_story_like',
+        'scheme: https',
+        'Accept:  /',
+        'Accept-Encoding: gzip, deflate, br',
+        'Accept-Language: en-US,en;q=0.9',
+        'Content-Type: application/x-www-form-urlencoded',
+        'Cookie: ' . $cookie,
+        'Dpr: 1',
+        'Origin: https://www.instagram.com',
+        'Referer: https://www.instagram.com/stories/khusaeni_al_baladi/3245879681262239508/',
+        'Sec-Ch-Prefers-Color-Scheme: light',
+        'Sec-Ch-Ua-Mobile: ?0',
+        'Sec-Ch-Ua-Model: ""',
+        'Sec-Ch-Ua-Platform: "Windows"',
+        'Sec-Ch-Ua-Platform-Version: "10.0.0"',
+        'Sec-Fetch-Dest: empty',
+        'Sec-Fetch-Mode: cors',
+        'Sec-Fetch-Site: same-origin',
+        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+        'Viewport-Width: 572',
+        'X-Asbd-Id: 129477',
+        'X-Csrftoken: ' . $csrf_token,
+        'X-Ig-App-Id: 936619743392459',
+        'X-Ig-Www-Claim: hmac.AR3IVhLVec5nNs2nFWWvasnRjopisvfQICgyUrev-YBxbwKW',
+        'X-Instagram-Ajax: 1010049462',
+        'X-Requested-With: XMLHttpRequest'
+    );
+
+    // foreach ($headers as &$header) {
+    //     if (strpos($header, 'x-csrf-token') !== false) {
+    //         $header = str_replace('x-csrf-token', 'X-Csrftoken', $header);
+    //         break;
+    //     }
+    // }
+
+    $postLikes = cURL("https://www.instagram.com/api/v1/story_interactions/send_story_like", "media_id=$media_id", $cookie, $headerss);
+    $postLikes = json_decode($postLikes["content"]);
+    if ($postLikes->status == 'ok') {
+        sleep(rand(3,6));
+        echo "Success like media_id: $media_id
+        ";
+    } else {
+        echo "Failed like media_id: $media_id
+        ";
+    }
+}
+
+function cURL($url, $fields = null, $cookie = null, $httpheader = null)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+    curl_setopt($ch, CURLOPT_REFERER, $url);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36');
+    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie);
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
+
+    if ($fields) {
+        $field_string = $fields;
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $field_string);
+    }
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $httpheader);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    $body = curl_exec($ch);
+    $err = curl_errno($ch);
+    $errmsg = curl_error($ch);
+    $response = curl_getinfo($ch);
+    curl_close($ch);
+
+    $response['errno'] = $err;
+    $response['errmsg'] = $errmsg;
+    $response['content'] = $body;
+    return $response;
+}
